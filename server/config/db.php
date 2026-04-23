44299bf10a9bc8fbe03c359c5f24e7c912960092
adee~ <113179431+Adk-gh@users.noreply.github.com>
2026-04-23 14:18:15 +0800

    db.config

--- server/config/db.php
+++ server/config/db.php
@@ -1,14 +1,15 @@
 <?php
 function getConnection(): mysqli {
-    $host   = 'localhost';
-    $user   = 'root';
-    $pass   = '';
-    $dbname = 'itprofel3';
+    $host = getenv('DB_HOST');
+    $user = getenv('DB_USER');
+    $pass = getenv('DB_PASS');
+    $db   = getenv('DB_NAME');
+    $port = getenv('DB_PORT') ?: 3306;  
 
     mysqli_report(MYSQLI_REPORT_OFF);
 
-    $conn = new mysqli($host, $user, $pass, $dbname);
-
+    $conn = new mysqli($host, $user, $pass, $db, $port);
+;
     if ($conn->connect_error) {
         http_response_code(500);
         header('Content-Type: application/json');
